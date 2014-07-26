480790718c5dc0b9ea11e4d78e9f41539bd3ca6f
App Academy Student <student@appacademy.io>
2014-07-26 00:31:38 -0400

    fixes where

--- lib/active_record_lite/02_searchable.rb
+++ lib/active_record_lite/02_searchable.rb
@@ -3,7 +3,6 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-<<<<<<< HEAD
     searchables = "#{params.keys.map { |key| key.to_s}.join(" = ? AND ")} = ?"
     query = DBConnection.execute(<<-SQL, *(params.values))
     SELECT
@@ -14,19 +13,13 @@ module Searchable
       #{searchables}
     SQL
     parse_all(query)
-=======
-    # ...
->>>>>>> 0cc231f222cf23d402157704302f6bf37b1944cb
   end
 end
 
 class SQLObject
-<<<<<<< HEAD
   extend Searchable
 end
 
 p Cat.where(:id=>1, :name=>"Breakfast")
-=======
   # Mixin Searchable here...
-end
->>>>>>> 0cc231f222cf23d402157704302f6bf37b1944cb
+
